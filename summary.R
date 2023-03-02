1dd99a521f2ef94d72ccea60dd11407a47128043
sophia-mangrubang <mangrsop@uw.edu>
2023-03-01 16:16:58 -0800

    finish r values

--- summary.R
+++ summary.R
@@ -16,6 +16,14 @@ View(SOUE_df)
 checkouts <- SOUE_df %>% group_by(Title)  %>% summarize(checkout_count = sum(Checkouts, na.rm = TRUE))
 View(checkouts)
 
+# Find most checked out book
+most_checkouts <- checkouts %>% filter(checkout_count == max(checkout_count, na.rm = TRUE)) %>% pull(Title)
+most_checkouts
+
+# highest total checkout count
+highest_checkouts <- checkouts %>% filter(checkout_count == max(checkout_count, na.rm = TRUE)) %>% pull(checkout_count)
+highest_checkouts
+
 # make a data table with checkouts for each audiobook in the series in 2022 & 2023
 SOUE_audio_df <- ls_book_df %>% filter(str_detect(Title, "Series")) %>% filter(str_detect(Title, "Unfortunate")) %>% filter(MaterialType == "AUDIOBOOK")
 View(SOUE_audio_df)
@@ -23,6 +31,27 @@ View(SOUE_audio_df)
 audio_checkouts <- SOUE_audio_df %>% group_by(Title)  %>% summarize(checkout_count = sum(Checkouts, na.rm = TRUE))
 View(audio_checkouts)
 
-# first book (The Bad Beginning) month with highest checkouts
-bb_highest_month <- SOUE_df %>% filter(str_detect(Title, "Beginning")) %>% summarize(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(CheckoutMonth)
+# first book(The Bad Beginning) month/year with highest ebook checkouts
+bb_highest_month <- SOUE_df %>% filter(str_detect(Title, "Beginning")) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(CheckoutMonth)
+bb_highest_year <- SOUE_df %>% filter(str_detect(Title, "Beginning")) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(CheckoutYear)
 bb_highest_month
+bb_highest_year
+
+ 
+#first book(The Bad Beginning) month/year with highest audiobook checkouts 
+bb_highest_audio_month <- SOUE_audio_df %>% filter(str_detect(Title, "Beginning")) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(CheckoutMonth)
+bb_highest_audio_year <- SOUE_audio_df %>% filter(str_detect(Title, "Beginning")) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(CheckoutYear)
+bb_highest_audio_month
+bb_highest_audio_year
+
+# Total checkouts in 2022
+total_checkouts_2022 <- SOUE_df %>% filter(CheckoutYear == "2022") %>% summarize(Checkouts = sum(Checkouts, na.rm = TRUE)) %>% pull(Checkouts)
+
+# Total checkouts in 2023
+total_checkouts_2023 <- SOUE_df %>% filter(CheckoutYear == "2023") %>% summarize(Checkouts = sum(Checkouts, na.rm = TRUE)) %>% pull(Checkouts)
+
+# change in (The Bad Beginning) ebook checkouts over time
+SOUE_df <- SOUE_df %>% filter(str_detect(Title, "Beginning")) %>% mutate(checkout_change = Checkouts - lag(Checkouts))
+
+
+
